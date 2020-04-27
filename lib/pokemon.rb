33aeb0f240dc4fec8d61dbd1456562ecffc86833
Michelle Lu <lu.michelle96@icloud.com>
2020-04-27 22:39:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,13 +2,13 @@ class Pokemon
   attr_reader :id, :name, :type, :hp, :db
   @@all = []
 
-  def initialize (id:, name:, type:, hp: , db:)
+  def initialize (id:, name:, type:, hp: nil , db:)
     @id = id
     @name = name
     @type = type
     @hp = hp
     @db = db
-    @@all << self
+    @all
   end
 
   def self.save(name, type, db)
